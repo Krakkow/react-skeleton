03645031ac7460f35262964edc863de22a5d5908
Krakkow <okowalsky@gmail.com>
2017-04-01 19:05:20 +0300

    ReactApp skeleton Ready
    This can be used to later application when I use ReactJS

---
+++ src/components/List.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var ListItem =  require('./ListItem.jsx');
+
+var ingredients = [{"id":1,"text":"item1"},{"id":2,"text":"item2"},{"id":3,"text":"item3"}];
+
+var List = React.createClass({
+  render: function() {
+    var listItems = ingredients.map(function(item) {
+      return <ListItem key={item.id} ingredient={item.text} />;
+    });
+
+    return (<ul>{listItems}</ul>);
+  }
+
+});
+
+module.exports = List;
